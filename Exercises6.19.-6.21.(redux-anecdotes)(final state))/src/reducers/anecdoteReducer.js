2e38ce62afa68f4873f6da65ef8a5c36aa41b041
Tekena Solomon <tekena157@gmail.com>
2020-07-21 07:10:03 +0100

    starting Exercises 6.19.-6.21.

---
+++ Exercises6.19.-6.21.(redux-anecdotes)(final state))/src/reducers/anecdoteReducer.js
@@ -0,0 +1,54 @@
+import anecdoteService from '../services/anecdotes'
+
+export const voteAnecdote = (anecdote) => {
+  return async (dispatch) => {
+    await anecdoteService.vote({
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    })
+    dispatch({
+      type: 'VOTE',
+      data: anecdote,
+    })
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.create(content)
+    dispatch({
+      type: 'CREATE',
+      data: newAnecdote,
+    })
+  }
+}
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    })
+  }
+}
+
+const anecdoteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'VOTE':
+      return state.map((anecdote) =>
+        anecdote.id === action.data.id
+          ? { ...anecdote, votes: anecdote.votes + 1 }
+          : anecdote
+      )
+
+    case 'CREATE':
+      return [...state, action.data]
+    case 'INIT_ANECDOTES':
+      return action.data
+    default:
+      return state
+  }
+}
+
+export default anecdoteReducer
---
+++ Exercises6.19.-6.21.(redux-anecdotes)(final state))/src/reducers/notificationReducer.js
@@ -0,0 +1,29 @@
+export const setNotification = (content, time) => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'ADD_NOTIF',
+      data: {
+        content,
+      },
+    })
+
+    setTimeout(() => {
+      dispatch({
+        type: 'REMOVE_NOTIF',
+      })
+    }, time * 1000)
+  }
+}
+
+const notificationReducer = (state = { message: ``, show: false }, action) => {
+  switch (action.type) {
+    case 'ADD_NOTIF':
+      return { message: `you voted ${action.data.content}`, show: true }
+    case 'REMOVE_NOTIF':
+      return { message: ``, show: false }
+    default:
+      return state
+  }
+}
+
+export default notificationReducer
